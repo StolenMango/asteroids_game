f1b5a703914c3e3aa5d519009da2563ff5f028c8
StolenMango <simionescuionut1@gmail.com>
2025-09-15 13:59:26 +0100

    player object

--- player.py
+++ player.py
@@ -1,4 +1,15 @@
 from circleshape import *
 
 Class Player(CircleShape):
-    def __init__(self, x, y):
+    def __init__(self, x, y, rotation):
+    super()def __init__(x, y)
+    self.rotation = 0
+
+# player will look like a triangle, even though we'll use a circle to represent its hitbox
+def triangle(self):
+    forward = pygame.Vector2(0, 1).rotate(self.rotation)
+    right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+    a = self.position + forward * self.radius
+    b = self.position - forward * self.radius - right
+    c = self.position - forward * self.radius + right
+    return [a, b, c]
