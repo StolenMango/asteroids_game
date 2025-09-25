6392a87a787304d9a0c047afed1468d0daa976cd
StolenMango <simionescuionut1@gmail.com>
2025-09-25 11:48:11 +0100

    Rotating (a and d)

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
-from constants import PLAYER_RADIUS
-from circleshape import CircleShape
 import pygame
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from circleshape import CircleShape
 
 class Player(CircleShape):
     def __init__(self, x, y,):
@@ -9,6 +9,17 @@ class Player(CircleShape):
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
 
 # player will look like a triangle, even though we'll use a circle to represent its hitbox
     def triangle(self):
