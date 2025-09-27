fbc488033a82bc4b30f4ceae2ada714b1ed632a1
StolenMango <simionescuionut1@gmail.com>
2025-09-27 21:40:03 +0100

    reorganising with groups

--- main.py
+++ main.py
@@ -5,13 +5,21 @@ import pygame
 from constants import *
 from player import Player
 
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+Player.containers = (updatable, drawable)
+
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
     while True:
         for event in pygame.event.get():
@@ -19,16 +27,12 @@ def main():
                 return
                 
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for drawing in drawable:
+            drawing.draw(screen)
         pygame.display.flip()
         clock.tick(60)
         dt = clock.get_time() / 1000  # in seconds
-    
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
 
 if __name__ == "__main__":
     main()
